0023415a715496a88e2a98ae7a789d5b5273a4df
Muhammad Imran Ismail <imranismail0610@gmail.com>
2025-12-11 11:41:10 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2368564789-bdb42",
+  "appId": "1:746063193956:web:a080d83c67b61c3879fc71",
+  "apiKey": "AIzaSyASWqESPzkatZoq0jowi8ICeMXf1PjV4SI",
+  "authDomain": "studio-2368564789-bdb42.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "746063193956"
+};
