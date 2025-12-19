6b9ca15ffec10a286aef59ad6e2153e32356f9c1
Muhammad Imran Ismail <imranismail0610@gmail.com>
2025-12-19 15:11:04 +0000

    Just don't use the drop-down menu for custom themes on mobile only

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -11,10 +11,6 @@ import {
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
-  DropdownMenuSub,
-  DropdownMenuSubTrigger,
-  DropdownMenuPortal,
-  DropdownMenuSubContent,
   DropdownMenuSeparator,
   DropdownMenuLabel,
 } from "@/components/ui/dropdown-menu"
@@ -23,51 +19,45 @@ export function ThemeToggle() {
   const { setTheme } = useTheme()
 
   return (
-    <div className="sm:hidden">
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant="ghost" size="icon">
-            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-            <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-            <span className="sr-only">Toggle theme</span>
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent align="end">
-          <DropdownMenuLabel>Appearance</DropdownMenuLabel>
-          <DropdownMenuItem onClick={() => setTheme("light")}>
-            Light
-          </DropdownMenuItem>
-          <DropdownMenuItem onClick={() => setTheme("dark")}>
-            Dark
-          </DropdownMenuItem>
-          <DropdownMenuItem onClick={() => setTheme("system")}>
-            System
-          </DropdownMenuItem>
-          <DropdownMenuSeparator />
-          <DropdownMenuSub>
-            <DropdownMenuSubTrigger>
-              <Palette className="mr-2 h-4 w-4" />
-              <span>Color Theme</span>
-            </DropdownMenuSubTrigger>
-            <DropdownMenuPortal>
-              <DropdownMenuSubContent>
-                <DropdownMenuItem onClick={() => setTheme("theme-default")}>
-                  Default (Rose)
-                </DropdownMenuItem>
-                <DropdownMenuItem onClick={() => setTheme("theme-forest")}>
-                  Forest
-                </DropdownMenuItem>
-                <DropdownMenuItem onClick={() => setTheme("theme-ocean")}>
-                  Ocean
-                </DropdownMenuItem>
-                 <DropdownMenuItem onClick={() => setTheme("theme-rose")}>
-                  Sakura
-                </DropdownMenuItem>
-              </DropdownMenuSubContent>
-            </DropdownMenuPortal>
-          </DropdownMenuSub>
-        </DropdownMenuContent>
-      </DropdownMenu>
-    </div>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" size="icon">
+          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuLabel>Appearance</DropdownMenuLabel>
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          System
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuLabel>
+          <div className="flex items-center gap-2">
+            <Palette className="h-4 w-4" />
+            <span>Color Theme</span>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuItem onClick={() => setTheme("theme-default")}>
+          Default (Rose)
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("theme-forest")}>
+          Forest
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("theme-ocean")}>
+          Ocean
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("theme-rose")}>
+          Sakura
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   )
 }
