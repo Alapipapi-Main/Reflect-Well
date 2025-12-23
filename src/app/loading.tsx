e9a48d73ef29a2b5c5541ffef4709a40e9711862
Muhammad Imran Ismail <imranismail0610@gmail.com>
2025-12-23 03:50:41 +0000

    I think you make the loading screen ui on another file

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,8 +1,9 @@
+
 import { Loader } from "lucide-react";
 
 export default function Loading() {
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex items-center justify-center h-screen bg-background">
       <Loader className="h-12 w-12 animate-spin text-primary" />
     </div>
   );
